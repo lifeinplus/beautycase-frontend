97cc75cdbd7f67235aa59d10d70989b8876210b2
Artem Denisov <lifeinplus@gmail.com>
2024-11-20 23:55:14 +0100

    Initial setup: Vite + Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => (
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+        <h1 className="text-4xl font-bold text-blue-500">
+            Welcome to Beauty Case!
+        </h1>
+    </div>
+);
+
+export default App;

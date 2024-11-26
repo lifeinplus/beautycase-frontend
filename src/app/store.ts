7e79a41f3b5b5e6520cf44e86119fdc23fa23f0a
Artem Denisov <lifeinplus@gmail.com>
2024-11-26 05:51:15 +0100

    BC-9 Theme Toggler

---
+++ src/app/store.ts
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit'
+import themeReducer from '../features/theme/themeSlice'
+
+export const store = configureStore({
+    reducer: { theme: themeReducer },
+    devTools: true,
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch

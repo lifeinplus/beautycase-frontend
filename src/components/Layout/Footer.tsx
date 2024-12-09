06014c814b47280772a83892d3f7b2213562a386
Artem Denisov <lifeinplus@gmail.com>
2024-12-09 08:51:39 +0100

    Bump version to 3.0.0

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -27,7 +27,7 @@ export const Footer = () => (
 
         <section className="mx-auto max-w-4xl p-4">
             <p>
-                v2.1.0 &copy; <span id="year">2024</span> Beautycase
+                v3.0.0 &copy; <span id="year">2024</span> Beautycase
             </p>
         </section>
     </footer>

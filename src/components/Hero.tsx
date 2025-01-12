e28c97e22e40867210aa040c35434199683dc245
Artem Denisov <lifeinplus@gmail.com>
2025-01-12 05:07:30 +0100

    BC-133 Refactor Hero component for better styling and responsive design

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,24 +1,19 @@
 interface HeroProps {
-    headline: string
+    headline?: string
     byline?: string
     imgUrl?: string
     content?: string
 }
 
 export const Hero = ({ headline, byline, imgUrl, content }: HeroProps) => (
-    <section
-        id="hero"
-        className="mb-6 flex flex-col items-center justify-center pt-6 sm:pt-0"
-    >
-        <div className="page-title">
-            <h1 className="page-headline">{headline}</h1>
-            {byline && <h2 className="page-byline">{byline}</h2>}
+    <section id="hero" className="hero">
+        <div className="hero-title">
+            {headline && <h1 className="hero-headline">{headline}</h1>}
+            {byline && <h2 className="hero-byline">{byline}</h2>}
         </div>
 
-        {imgUrl && (
-            <img alt={headline} className="mt-6 sm:rounded" src={imgUrl} />
-        )}
+        {imgUrl && <img alt={headline} className="hero-img" src={imgUrl} />}
 
-        {content && <p className="me-3 ms-3 mt-6">{content}</p>}
+        {content && <p className="hero-content">{content}</p>}
     </section>
 )

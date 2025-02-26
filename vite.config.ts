c4d9134b38ed440c54ce234877d3012e197ed3af
Artem Denisov <lifeinplus@gmail.com>
2025-02-26 15:42:55 +0100

    BC-180 Refactor testing setup and add new hooks tests for useRefreshAuth and useScrollToElement

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     test: {
         globals: true,
         environment: 'jsdom',
-        setupFiles: './tests/setup.js',
+        setupFiles: './tests/setup.ts',
         coverage: {
             exclude: ['**/*/index.{js,ts}', '**/*/index.{jsx,tsx}'],
         },

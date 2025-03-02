b7154ec6190fb72ddce4d60604522961e445c0b8
Artem Denisov <lifeinplus@gmail.com>
2025-03-02 19:00:11 +0100

    BC-190 Refactor LessonForm and MakeupBagForm components by simplifying field rendering

--- src/features/form/types.ts
+++ src/features/form/types.ts
@@ -1,14 +1,3 @@
-export interface FieldConfig<T> {
-    label: string
-    name: keyof T
-    type: 'button-navigate' | 'imageUrl' | 'select' | 'text' | 'textarea'
-    options?: SelectOption[]
-    path?: string
-    preview?: boolean
-    required?: boolean
-    rows?: number
-}
-
 export interface FormRef {
     focusInput: () => void
 }

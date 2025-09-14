18b2aed3ae3e2a035842284dd218ecc807c52cd2
Artem Denisov <lifeinplus@gmail.com>
2025-09-14 19:47:11 +0200

    BC-448 Refactor layout styles to improve spacing on iPhones

--- src/shared/components/layout/header/Header.tsx
+++ src/shared/components/layout/header/Header.tsx
@@ -8,7 +8,7 @@ import { LogoLink } from '../../ui/logo-link/LogoLink'
 
 export const Header = () => (
     <nav className="sticky top-0 z-10 border-b border-neutral-300 dark:border-neutral-700 sm:hidden">
-        <header className="mx-auto flex max-w-4xl items-center justify-between bg-white pe-0 ps-4 dark:bg-black">
+        <header className="pt-safe-top mx-auto flex max-w-4xl items-center justify-between bg-white pe-0 ps-4 dark:bg-black">
             <h1 className={classNames(logoStyles.logo, 'mb-1.5')}>
                 <LogoLink />
             </h1>
